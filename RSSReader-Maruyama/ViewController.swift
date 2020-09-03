d50655e3a2037a03912df9d8bdf62502a2cf028f
s_mrym <s.maruyama@oyster-bro.com>
2020-09-03 13:57:11 +0900

    Initial Commit

---
+++ RSSReader-Maruyama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSReader-Maruyama
+//
+//  Created by 丸山翔 on 2020/09/03.
+//  Copyright © 2020 丸山翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
