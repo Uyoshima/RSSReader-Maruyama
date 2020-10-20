47b74b95270ea5dc1e22216c1c56263087fc8f68
s_mrym <s.maruyama@oyster-bro.com>
2020-10-20 18:51:44 +0900

    Remove unused functions in ItemRepository

--- RSSReader-Maruyama/Repositories/ItemRepository.swift
+++ RSSReader-Maruyama/Repositories/ItemRepository.swift
@@ -43,24 +43,6 @@ class ItemRepository {
         }
     }
     
-    func deleteOtherThanReadLater() {
-        let deleteItems = realm.objects(Item.self).filter(isNotReadLaterPredicate)
-        try! realm.write {
-            realm.delete(deleteItems)
-        }
-    }
-    
-    func hasItem(_ item: Item) -> Bool {
-        let predicate = NSPredicate(format: "url == %@", item.url)
-        let results = realm.objects(Item.self).filter(predicate)
-        
-        if results.count == 0 {
-            return false
-        } else {
-            return true
-        }
-    }
-    
     /// 既に保存されている記事かを確認する。
     /// - Parameter item: 確認したいItem
     /// - Returns: 持っている→DBのオブジェクト、持っていない→引数のItem
@@ -73,16 +55,5 @@ class ItemRepository {
         } else {
             return results.first!
         }
-    }
-    
-    func getAllReadLaterItems() -> [Item] {
-        let results = realm.objects(Item.self).filter(isReadLaterPredicate)
-        
-        var items: [Item] = []
-        for item in results {
-            items.append(item)
-        }
-        
-        return items
     }
 }
